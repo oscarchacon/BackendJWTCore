18afc7e634e9af7346ea60bbccf88589fb1dc573
Oscar Chacon <oscarchaconcautin@gmail.com>
2019-04-02 11:41:17 -0300

    Cambios Menores, Implementacion de parametros de Auth0

--- BackJWTAuth0Core/Startup.cs
+++ BackJWTAuth0Core/Startup.cs
@@ -80,9 +80,11 @@ namespace BackJWTAuth0Core
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
+                app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseStaticFiles();
             app.UseAuthentication();
             app.UseCors("EnableCORS");
 
